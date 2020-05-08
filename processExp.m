b664ab7dccd22e80393271e5877f687d9aea92ba
Andres Florez <andrewflorez@gmail.com>
2020-05-07 21:07:51 -0400

    Improve readibility of Macros

--- processExp.m
+++ processExp.m
@@ -208,9 +208,9 @@ BatchSuperSeggerOpti( dirname, skip, cleanflag, CONST,startEnd);
 
 %% Converting images to stack 
 
-disp('Converting Images to Stack');
-macroFile2='tifImagesToStack.txt';
-runMacro([filepathMacro,macroFile2],dirname);
+% disp('Converting Images to Stack');
+% macroFile2='tifImagesToStack.txt';
+% runMacro([filepathMacro,macroFile2],dirname);
 
 %% 3. Run Morphometrics for Pill Mesh calculation 
 
@@ -220,31 +220,31 @@ runMacro([filepathMacro,macroFile2],dirname);
 % Morphometrics to quickly re-segment the binary masks, contour
 % fitting, mesh calculation together with basic lineage tracking.
 
-paramName ='Morphometrics_prefs_mask_CL'; %Select parameter file 
-params = loadParams( paramName );
-
-%List of most frequently changed parameters, modify here for different
-%images
-params.v_imtype = 2;        % 1 = Phase; 2 = Fluorescence (internal); 3 = Fluorescence (peripheral)      
-params.v_method = 3;        % 1 = Gradient Segmentation; 2 = Laplacian Segmentation; 
-                            % 3 = Adaptive Threshold Segmentation; 4 = Canny Segmentation
-params.v_simplethres=1;     % Simple threshold 
-params.f_areamin = 10;     % Min region size
-params.f_areamax = 200000;  % Max region size
-params.v_prox = 0;          % Cells are in proximity
-params.v_exclude=0;         % Exclude edge objects
-params.f_hmin_split=2;      % Cut distance (pxls)
-params.v_save = 1;          % Save output
-params.v_mt_mesh=1;         % Pill mesh
-params.v_falsepos = 1;      % Reject false positives
-params.f_int_rej = 3;       % False positive rejection parameter
-% Tracking parameters:
-params.f_pert_same = 0.55;  % Fractional overlap
-params.f_frame_diff = 4;    % Frame overlap
-%workers = 6;                % Number of workers for parallel job
-
-disp('Running Morphometrics in parallel')
-run_parallel(dirname,params);
+% paramName ='Morphometrics_prefs_mask_CL'; %Select parameter file 
+% params = loadParams( paramName );
+% 
+% %List of most frequently changed parameters, modify here for different
+% %images
+% params.v_imtype = 2;        % 1 = Phase; 2 = Fluorescence (internal); 3 = Fluorescence (peripheral)      
+% params.v_method = 3;        % 1 = Gradient Segmentation; 2 = Laplacian Segmentation; 
+%                             % 3 = Adaptive Threshold Segmentation; 4 = Canny Segmentation
+% params.v_simplethres=1;     % Simple threshold 
+% params.f_areamin = 10;     % Min region size
+% params.f_areamax = 200000;  % Max region size
+% params.v_prox = 0;          % Cells are in proximity
+% params.v_exclude=0;         % Exclude edge objects
+% params.f_hmin_split=2;      % Cut distance (pxls)
+% params.v_save = 1;          % Save output
+% params.v_mt_mesh=1;         % Pill mesh
+% params.v_falsepos = 1;      % Reject false positives
+% params.f_int_rej = 3;       % False positive rejection parameter
+% % Tracking parameters:
+% params.f_pert_same = 0.55;  % Fractional overlap
+% params.f_frame_diff = 4;    % Frame overlap
+% %workers = 6;                % Number of workers for parallel job
+% 
+% disp('Running Morphometrics in parallel')
+% run_parallel(dirname,params);
 
 %% 4. Foci calculation - Diego's pipeline
 
@@ -277,8 +277,8 @@ rmdir( [dirname,filesep,'raw_im',filesep],'s' );
 
 %% Shutting down parallel pool
 
-poolobj = gcp('nocreate');
-delete(poolobj);
+% poolobj = gcp('nocreate');
+% delete(poolobj);
 
 
 %% THE END
