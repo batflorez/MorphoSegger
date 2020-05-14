ab34c93be0e176860f7cc18de6a56aaaf3cd1df2
saraiza <saraiza@908devices.com>
2020-05-13 20:22:39 -0400

    First draft from Diego

---
+++ FociAnalysis/Analysis.m
@@ -0,0 +1,136 @@
+tic
+clear all
+
+param_fit=50; %% Minimo de puntos para considerar el fit exponencial valido. 
+
+list_1 = dir('*xy*');% todas las xy carpetas
+ 
+%parfor?
+for p=1:1:length(list_1)
+ 
+            name_root=list_1(p).name; 
+    
+            name_folder=strcat(name_root,'/morphometrics');
+          
+
+            cd(name_folder)
+
+            list_2 = dir('*pill_MESH.mat'); %pill mesh file
+            
+            name_morph=list_2(1).name;
+
+            load(name_morph);
+            
+            %%%%% Encuentra el frame inicial y final por celula con la
+            %%%%% mayor longitud in-interrumpida de frames. Genera la matrix
+            %%%%% limites. Cada fila es una celula. Esto podria ser una
+            %%%%% funcion?
+            
+            %%%%% Inicia aca
+            
+            ind=zeros(Nframe,Ncell);
+
+            cont=zeros(Ncell, Nframe);
+
+            limites=zeros(Ncell,2);
+
+            for N=1:1:Ncell
+
+                for fr=1:1:Nframe
+
+                   allCN = [frame(fr).object.cellID]; % comma separated list expansion 
+                    ind = find(allCN == N);
+
+                     aux = isempty(ind);
+
+                     if aux==1
+
+                     cont(N,fr)=-10;    
+
+                     end 
+
+
+                end
+            end  
+
+
+
+            for j=1:1:Ncell
+
+                 maximo=Nframe;
+                 min=1;
+
+
+                 temp=find(cont(j,:)==0);
+
+                 if isempty(temp)==1
+
+                     continue
+
+                 else
+
+                 loc=find(diff(temp)~=1);
+
+                 if isempty(loc)==1
+            %          
+
+                    limites(j,1)=temp(1);
+                    limites(j,2)=temp(end);
+
+                    continue
+            %          
+                 else
+
+                    delta=[temp(1),temp(loc),temp(loc(end)+1),temp(end)];
+
+                    dif_d=diff(delta);
+
+                    pos=find(dif_d==max(dif_d));
+
+
+                    limites(j,1)=delta(pos(1));
+                    limites(j,2)=delta(pos(1)+1);
+
+                  end
+
+
+                 end
+
+            end
+
+            %%%% Termina aca. 
+            
+            
+            salida=fun_anal4N(Ncell,frame,name_morph,limites,param_fit);
+
+            %fun_ph_growth(Ncell,name,size(tsStack,2),limites,param_fit);
+            
+            cd ../..
+            
+            %cd('cell_cycle')
+%             
+            name=name_morph(1:end-4);
+%             
+            arch_nombre=strcat(name,'_OUTPUT_FOCI.mat');
+%     
+            arch_nombre=strcat('cell_cycle/',arch_nombre);
+            
+            save(arch_nombre,'salida');
+            
+            %cd ..
+%            
+            clear frame
+            
+           
+     
+    
+     
+
+     
+ end       
+            
+            
+clear all
+
+toc
+
