36eae1b578ce95c6f4215b35dc28e32bfe3235a9
Andres Florez <andrewflorez@gmail.com>
2022-07-26 20:28:06 -0400

    Update BatchSuperSeggerOpti.m

--- Supersegger-omnipose/SuperSegger-master/batch/BatchSuperSeggerOpti.m
+++ Supersegger-omnipose/SuperSegger-master/batch/BatchSuperSeggerOpti.m
@@ -219,8 +219,8 @@ else
         cleanup = onCleanup( @()( delete( h ) ) );
     end
     
-    parfor(j = 1:num_xy,workers)
-    %for j = 1:num_xy
+    %parfor(j = 1:num_xy,workers)
+    for j = 1:num_xy
         
         dirname_xy = dirname_list{j};
         intProcessXY( dirname_xy, skip, nc, num_c, clean_flag, ...
@@ -393,8 +393,8 @@ disp('Continuing segmentation.');
 % Edit: not sure why this was at 2... 3 is segmentation 
 %if startEnd(1) <= 3 && startEnd(2) >=2 && ~exist( stamp_name, 'file' )
 if startEnd(1) <= 2 && startEnd(2) >=2 && ~exist( stamp_name, 'file' )
-   parfor(i=1:num_t,workers) % through all frames
-    %for i = 1:num_t
+   %parfor(i=1:num_t,workers) % through all frames
+    for i = 1:num_t
         
         if isempty( crop_box )
             crop_box_tmp = [];
